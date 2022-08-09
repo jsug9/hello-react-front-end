dc220456ed59f31d31b66a7079e69e5c7fa47c28
Augusto <jesus_augga_99@icloud.com>
2022-08-09 12:37:32 -0500

    add routes to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import GreetingPage from './pages/GreetingPage';
+
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <p>
-        Edit
-        {' '}
-        <code>src/App.js</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <Router basename={process.env.PUBLIC_URL}>
+    <Routes>
+      <Route path="/" element={<GreetingPage />} />
+    </Routes>
+  </Router>
 );
 
 export default App;
