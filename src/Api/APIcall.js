19f228f808c4cab26d6c3f7710033572b97f6132
Augusto <jesus_augga_99@icloud.com>
2022-08-09 12:55:38 -0500

    rename greeting call to avoid conflicts

--- src/Api/APIcall.js
+++ src/Api/APIcall.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const getGreeting = async () => {
+const greetingRequest = async () => {
   const response = await axios.get('http://localhost:3000/v1/greetings');
   return response.data;
 };
 
-export default getGreeting;
+export default greetingRequest;
--- src/Redux/greeting/greeting.js
+++ src/Redux/greeting/greeting.js
@@ -4,6 +4,14 @@ const initialState = {
 
 const GET_GREETING = '/greeting/GET_GREETING';
 
+const getGreeting = async (dispatch) => {
+  const greeting = { greeting: 'Hello World' };
+  return dispatch({
+    type: GET_GREETING,
+    payload: greeting,
+  });
+};
+
 const greetingReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_GREETING:
@@ -14,5 +22,5 @@ const greetingReducer = (state = initialState, action) => {
   }
 };
 
-export { GET_GREETING };
+export { getGreeting };
 export default greetingReducer;
